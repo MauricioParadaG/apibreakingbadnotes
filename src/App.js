eff673bc87c6e6866538e31d5cfc97247c281643
MauricioParadaG <62025899+MauricioParadaG@users.noreply.github.com>
2020-06-14 09:55:10 +0200

    Small project, connecting to a API and getting some Json information. with Styled Component CSS
    Small project, connecting to a API and getting some Json information. with Styled Component CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import styled from '@emotion/styled';
+
+const DivContainer = styled.div`
+    display: flex;
+    align-items: center;
+    padding-top: 5rem;
+    flex-direction: column;
+`; 
+
+const ButtonNotes = styled.button`
+   background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+   background-size: 300px;
+   font-family: Arial, Helvetica, sans-serif;
+   color: #fff;
+   margin-top: 3rem;
+   padding: 1rem 3rem;
+   font-size: 2rem;
+   border: 2px solid black;   
+
+   &:hover{
+        cursor: pointer;
+    }
+`; 
+
+
+
+
 
 function App() {
+
+  const [breakNote, setBreakNoteState] = useState({});
+
+  const checkingApi = async () =>{
+    const res =  await fetch('http://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const data = await res.json();
+    
+    setBreakNoteState(data[0]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DivContainer>
+      <ButtonNotes
+        onClick={() => checkingApi()}
+      >
+        Break it bad
+      </ButtonNotes>
+    </DivContainer>
   );
 }
 
